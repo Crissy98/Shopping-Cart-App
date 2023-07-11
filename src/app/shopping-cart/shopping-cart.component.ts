4bcd1b26fe8045df47b102f13f98732c8f590db8
Crissy98 <mihailacristina98@gmail.com>
2023-07-11 08:23:08 +0300

    first commit

--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,13 +1,19 @@
 import { Component} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
+
+
 export class ShoppingCartComponent{
-  
+
+  items = this.cartService.getItems();
+
   constructor(
-    private route: ActivatedRoute) {}
+    private route: ActivatedRoute,private cartService: CartService) {}
 }
+
